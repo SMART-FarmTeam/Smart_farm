ba16622ab474a35190d8a5fce51bc370d78e605e
HanKwangJin <97279506+HanKwangJin@users.noreply.github.com>
2022-04-26 17:12:38 +0900

    Delete smartFarm directory

--- smartFarm/src/main/java/com/smartFarm/project/model/smartFarm/SmartFarmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smartFarm.project.model.smartFarm;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SmartFarmRepository extends JpaRepository<SmartFarmVo, String>{
-
-}
